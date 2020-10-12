0f6a344898a7b1cbf351d373f4e63ecd00c1b80a
Ahmadreza <ahmadreza.saboor2012@gmail.com>
2020-10-12 20:31:54 +0330

    parameterize field names

--- src/structural_feature_extractor.py
+++ src/structural_feature_extractor.py
@@ -1,77 +1,86 @@
 
 class StructuralFeatureExtractor:
-	def __init__(self, db, lang):
-		self.db = db
-		self.lang = lang
+  ID_FIELD = "Id"
+  NAME_FIELD = "Name"
+  FILE_PATH_FIELD = "FilePath"
 
-	def get_ents(self):
-		pass
+  def __init__(self, db, lang):
+    self.db = db
+    self.lang = lang
 
-	def get_dependencies(self, entity):
-		pass
+  def get_ents(self):
+    pass
 
-	def extract_metadata(self):
-		metadata = {}
-		metadata.setdefault("id", [])
-		metadata.setdefault("name", [])
-		entities = self.get_ents()
-		for entity in entities:
-			metadata["id"].append(entity.id())
-			metadata["name"].append(entity.name())
-			metric_names = entity.metrics()
-			metrics = entity.metric(metric_names)
-			for name, value in metrics.items():
-				metadata.setdefault(name, [])
-				metadata[name].append(value)
-		return metadata
+  def get_dependencies(self, entity):
+    pass
 
-	def extract_dependency_graph(self):
-		graph = {}
-		entities = self.get_ents()
-		ent_id_set = set(map(lambda e: e.id(), entities))
-		for entity in entities:
-			for ref in self.get_dependencies(entity):
-				if ref.ent().id() in ent_id_set:
-					graph.setdefault(entity.id(), [])
-					graph[entity.id()].append(ref.ent().id())
-		return graph
+  def extract_metadata(self):
+    metadata = {}
+    metadata.setdefault(StructuralFeatureExtractor.ID_FIELD, [])
+    metadata.setdefault(StructuralFeatureExtractor.NAME_FIELD, [])
+    entities = self.get_ents()
+    for entity in entities:
+      metadata[StructuralFeatureExtractor.ID_FIELD].append(entity.id())
+      metadata[StructuralFeatureExtractor.NAME_FIELD].append(entity.name())
+      metric_names = entity.metrics()
+      metrics = entity.metric(metric_names)
+      for name, value in metrics.items():
+        metadata.setdefault(name, [])
+        metadata[name].append(value)
+    return metadata
+
+  def extract_dependency_graph(self):
+    graph = {}
+    entities = self.get_ents()
+    ent_id_set = set(map(lambda e: e.id(), entities))
+    for entity in entities:
+      for ref in self.get_dependencies(entity):
+        if ref.ent().id() in ent_id_set:
+          graph.setdefault(entity.id(), [])
+          graph[entity.id()].append(ref.ent().id())
+    return graph
 
 
 class FileStructuralFeatureExtractor(StructuralFeatureExtractor):
-	def get_ents(self):
-		return self.db.ents(f"{self.lang} file ~unresolved ~unknown")
+  def get_ents(self):
+    return self.db.ents(f"{self.lang} file ~unresolved ~unknown")
 
-	def get_dependencies(self, entity):
-		return entity.refs(f'{self.lang} include')
+  def get_dependencies(self, entity):
+    return entity.refs(f'{self.lang} include')
 
-	def extract_metadata(self):
-		metadata = StructuralFeatureExtractor.extract_metadata(self)
-		metadata.setdefault("file_path", [])
-		ids = metadata["id"]
-		for id in ids:
-			entity = self.db.ent_from_id(id)
-			metadata["file_path"].append(entity.relname())
-		return metadata
+  def extract_metadata(self):
+    metadata = StructuralFeatureExtractor.extract_metadata(self)
+    metadata.setdefault(StructuralFeatureExtractor.FILE_PATH_FIELD, [])
+    ids = metadata[StructuralFeatureExtractor.ID_FIELD]
+    for id in ids:
+      entity = self.db.ent_from_id(id)
+      metadata[StructuralFeatureExtractor.FILE_PATH_FIELD].append(entity.relname())
+    return metadata
 
 
 class FunctionStructuralFeatureExtractor(StructuralFeatureExtractor):
-	def get_ents(self):
-		return self.db.ents(f"{self.lang} function ~unresolved ~unknown")
+  FULL_NAME_FIELD = "FullName"
+  PARAMETERS_FIELD = "Parameters"
 
-	def get_dependencies(self, entity):
-		return entity.refs(f'{self.lang} call')
+  def get_ents(self):
+    return self.db.ents(f"{self.lang} function ~unresolved ~unknown")
 
-	def extract_metadata(self):
-		metadata = StructuralFeatureExtractor.extract_metadata(self)
-		metadata.setdefault("file_path", [])
-		metadata.setdefault("full_name", [])
-		metadata.setdefault("parameters", [])
-		ids = metadata["id"]
-		for id in ids:
-			entity = self.db.ent_from_id(id)
-			metadata["full_name"].append(entity.longname())
-			define_in_ref = entity.ref("definein")
-			metadata["file_path"].append(None if define_in_ref is None else define_in_ref.file().relname())
-			parameters = entity.parameters(False)
-			metadata["parameters"].append(None if not parameters else parameters)
-		return metadata
+  def get_dependencies(self, entity):
+    return entity.refs(f'{self.lang} call')
+
+  def extract_metadata(self):
+    metadata = StructuralFeatureExtractor.extract_metadata(self)
+    metadata.setdefault(StructuralFeatureExtractor.FILE_PATH_FIELD, [])
+    metadata.setdefault(FunctionStructuralFeatureExtractor.FULL_NAME_FIELD, [])
+    metadata.setdefault(FunctionStructuralFeatureExtractor.PARAMETERS_FIELD, [])
+    ids = metadata[StructuralFeatureExtractor.ID_FIELD]
+    for id in ids:
+      entity = self.db.ent_from_id(id)
+      metadata[FunctionStructuralFeatureExtractor.FULL_NAME_FIELD].append(entity.longname())
+      define_in_ref = entity.ref("definein")
+      metadata[StructuralFeatureExtractor.FILE_PATH_FIELD].append(
+          None if define_in_ref is None else define_in_ref.file().relname()
+      )
+      parameters = entity.parameters(False)
+      metadata[FunctionStructuralFeatureExtractor.PARAMETERS_FIELD].append(None if not parameters else parameters)
+    return metadata
