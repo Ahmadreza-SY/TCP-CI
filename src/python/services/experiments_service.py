e0fd61b7dbc526da77b6a245472f5f97ba8fab41
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2021-08-22 13:13:24 -0400

    remove non-failed builds after removing outlier tests

--- src/python/services/experiments_service.py
+++ src/python/services/experiments_service.py
@@ -28,15 +28,19 @@ class ExperimentsService:
         outliers = []
         for _, r in test_fcount.iterrows():
             if abs(r["rate"] - mean) > 3 * std:
-                outliers.append(r[Feature.TEST])
+                outliers.append(int(r[Feature.TEST]))
 
         outliers_path = (
             args.output_path / "tsp_accuracy_results" / "full-outliers" / "outliers.csv"
         )
         outliers_path.parent.mkdir(parents=True, exist_ok=True)
         pd.DataFrame({"test": outliers}).to_csv(outliers_path, index=False)
+        result_df = dataset_df[~dataset_df[Feature.TEST].isin(outliers)]
+        failed_builds = (
+            result_df[result_df[Feature.VERDICT] > 0][Feature.BUILD].unique().tolist()
+        )
         return (
-            dataset_df[~dataset_df[Feature.TEST].isin(outliers)]
+            result_df[result_df[Feature.BUILD].isin(failed_builds)]
             .copy()
             .reset_index(drop=True)
         )
